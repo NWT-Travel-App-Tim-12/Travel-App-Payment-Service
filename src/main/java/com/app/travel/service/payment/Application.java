73aa6a94b986f62460248be172f2874beaf4ab0d
Ahmed <amahovac1@etf.unsa.ba>
2023-04-07 11:35:26 +0200

    changed config

--- src/main/java/com/app/travel/service/payment/Application.java
+++ src/main/java/com/app/travel/service/payment/Application.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @OpenAPIDefinition
