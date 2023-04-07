dba83911e1babf7df2d2185b87dcbb24dc988689
Ahmed <amahovac1@etf.unsa.ba>
2023-04-07 07:50:52 +0200

    added discovery client

--- src/main/java/com/app/travel/service/payment/Application.java
+++ src/main/java/com/app/travel/service/payment/Application.java
@@ -3,9 +3,12 @@ package com.app.travel.service.payment;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @OpenAPIDefinition
+@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) {
